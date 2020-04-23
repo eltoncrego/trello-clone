9c1c589c71b4f2c56eaab07bc13d63e0ba8f7428
Elton C. Rego <rego.elton@gmail.com>
2020-04-23 13:23:33 -0700

    Get rid of warnings

--- client/src/Shared/Constants/PlaygroundComponent.js
+++ client/src/Shared/Constants/PlaygroundComponent.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 // This component is for defining what components 
 // we want to test in the Playground.
-// Replace `null` with the component that you want to test!
+// Fill the fragment with the component that you want to test!
 // Make sure to enable the feature flag in FeatureFlags.js...
-export const PlaygroundComponent = () => (null);
+export const PlaygroundComponent = () => (
+  <>
+  </>
+);
