d56e3f6d7737f5688c1869c924b977ea203c3368
Elton C. Rego <rego.elton@gmail.com>
2020-04-24 09:13:49 -0700

    Remove stories for pages

--- client/src/Views/PageError/PageError.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PageError from './PageError';
-
-export default {
-  component: PageError,
-  title: 'Page Error'
-};
-
-export const ErrorPage = () => <PageError/>
