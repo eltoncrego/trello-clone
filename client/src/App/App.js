71685f7ece0319e3fba016de7cf17aa27ec667a7
Elton C. Rego <rego.elton@gmail.com>
2020-04-23 20:16:06 -0700

    Get rid of playground and get ready for storybook

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,15 +3,13 @@ import NormalizeStyles from './NormalizeStyles';
 import BaseStyles from './BaseStyles';
 import './FontStyles.css';
 
-import { featureFlags } from '../Shared/Constants/FeatureFlags';
-import Playground from '../Shared/Components/Playground/Playground';
 import Routes from './Routes';
 
 const App = () => (
   <>
     <NormalizeStyles/>
     <BaseStyles/>
-    {featureFlags.playground ? <Playground/> : <Routes/>}
+    <Routes/>
   </>
 );
 
