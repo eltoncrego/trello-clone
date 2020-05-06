19467e9105f830efbfbe4f95ddd3596badf49f71
Elton C. Rego <rego.elton@gmail.com>
2020-05-06 16:19:31 -0700

    Create basic react application from scratch

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js',
+  },
+  devServer: {
+    inline: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
